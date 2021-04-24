4327ef54a4f75773d5242b62b8b6b84901b1854e
sshahram <shirin.shahram23@gmail.com>
2021-04-24 00:16:40 -0700

    add findAll to category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,20 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    // be sure to include its associated Products
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name','price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
