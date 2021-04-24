1de3b9766bc60a12980495a86688ac9b773e624f
sshahram <shirin.shahram23@gmail.com>
2021-04-23 23:39:25 -0700

    add association between Product & Category

--- models/index.js
+++ models/index.js
@@ -5,8 +5,14 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
 
